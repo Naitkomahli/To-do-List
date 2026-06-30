128f8e4b6f3af93f6514d2df6f3f9c620d739eb5
Naitkomahli <ilhanoktian31@gmail.com>
2026-06-30 16:53:24 +0700

    fix: dynamic base path for Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/To-do-List/',
+  base: process.env.VERCEL ? '/' : '/To-do-List/',
   plugins: [
     react(),
     tailwindcss(),
